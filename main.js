bd173184b23c73bf04893adbed1103fc6fd65f93
telewi <watelewi96.com>
2023-02-22 21:12:53 +0800

    add contact

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ function toggleDetail(e) {
 
     $(detail).slideToggle()
 }
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("inp_email")
+    const subject = $("inp_subject")
+    const message = $("inp_message")
+    if(!$(email).val()) {
+        alert("Email is required")
+    } else if (!$(subject).val()) {
+        alert("Email is required")
+    } else if (!$(message).val()) {
+        alert("Email is required")
+    } else{
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
+}
